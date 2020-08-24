eaffcb6602b209c9447c925d720fa341c5dcd369
sillyhat <16219987+sillyhatxu@users.noreply.github.com>
2020-08-24 22:24:09 +0800

    test build sql

--- dml_test.go
+++ dml_test.go
@@ -1,31 +1,43 @@
 package mysqlclient
 
 import (
+	"github.com/sillyhatxu/db-client/builder"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
 )
 
+type User struct {
+	Id               int64      `column:"id"`
+	LoginName        string     `column:"login_name"`
+	Password         string     `column:"password"`
+	UserName         string     `column:"user_name"`
+	Status           bool       `column:"status"`
+	Platform         string     `column:"platform"`
+	Age              *int       `column:"age"`
+	Amount           *float64   `column:"amount"`
+	Desc             *string    `column:"Description"`
+	Birthday         *time.Time `column:"birthday"`
+	CreatedTime      time.Time  `column:"created_time"`
+	LastModifiedTime time.Time  `column:"last_modified_time"`
+}
+
 func TestMysqlClient_Insert(t *testing.T) {
+	once.Do(setup)
+	data := []map[string]interface{}{
+		{
+			"foo": "bar",
+			"age": 23,
+		},
+	}
+	//TODO struct to map
+	sql, args, err := builder.BuildInsert("user_info", data)
+	assert.Nil(t, err)
 
 }
 
 func TestMysqlClient_Find(t *testing.T) {
 	once.Do(setup)
-	type User struct {
-		Id               int64      `column:"id"`
-		LoginName        string     `column:"login_name"`
-		Password         string     `column:"password"`
-		UserName         string     `column:"user_name"`
-		Status           bool       `column:"status"`
-		Platform         string     `column:"platform"`
-		Age              *int       `column:"age"`
-		Amount           *float64   `column:"amount"`
-		Desc             *string    `column:"Description"`
-		Birthday         *time.Time `column:"birthday"`
-		CreatedTime      time.Time  `column:"created_time"`
-		LastModifiedTime time.Time  `column:"last_modified_time"`
-	}
 	var userArray []User
 	sql := "select * from user"
 	err := mysqlClient.Find(sql, &userArray)
